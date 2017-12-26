9f008f9e99c413268e5374e0914e8bec094c9b12
Sprax Lines <sprax@talla.com>
2017-12-26 14:51:34 -0500

    notes on filter order

--- es_analyze.sh
+++ es_analyze.sh
@@ -2,22 +2,28 @@
 # See: https://stackoverflow.com/questions/17029902/using-curl-post-with-variables-defined-in-bash-script-functions
 
 operations=analyze
-query_text=${1:-"The YeLLoWing café beLLows was sleeping FURIOUSly."}
+query_text=${1:-"The YeLLoWing café beLLows MiCe were sleeping FURIOUSly."}
 prettytrue=${2:-true}
 porterstem=${3:-true}
 do_explain=${4:-false}
 tokenizer1=${5:-standard}
 
+# NOTES: filters are applied in the order listed, so stemming exceptions (keywords)
+# and overrides must be listed before the stemmer, and overrides that convert other
+# words into to stop words should precede stopword removal.
 gen_data()
 {
 cat <<EOF
 {
     "tokenizer": "$tokenizer1",
-    "filter": [ "lowercase", "asciifolding", "porter_stem",
-                    {"type": "stop", "stopwords": ["a", "is", "the"]}
+    "filter": [ "lowercase", "asciifolding",
+                {"type": "keyword_marker", "keywords": ["sleeping"]},
+                {"type": "stemmer_override", "rules": [ "mice=>mouse", "were=>was"]},
+                {"type": "stop", "stopwords": ["a", "is", "the", "was"]},
+                "porter_stem"
               ],
     "text": "$query_text",
-    "explain" : "$do_explain"
+    "explain": "$do_explain"
 }
 EOF
 } 
