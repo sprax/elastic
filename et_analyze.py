eaf536be37216696324315c59fcc0edbef1b662f
Sprax Lines <sprax@talla.com>
2017-12-28 16:33:07 -0500

    requests_get_es

--- et_analyze.py
+++ et_analyze.py
@@ -74,35 +74,47 @@ def json_text(text_to_analyze=DEFAULT_TEXT):
 #
 # For your specific curl translation:
 
-def post_es(payload=None, pretty=PRETTY, verbose=0):
+def requests_post_es(payload=None, pretty=PRETTY, verbose=0):
     '''post request to Elasticsearch'''
     url = "http://localhost:9200/_analyze?pretty=%s" % pretty
     payload = payload if payload else json_text()  # json_data()
     headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     results = requests.post(url, data=payload, headers=headers)
     if verbose:
-        print("post_es got results of type(%s): (%s)" % (type(results).__name__, results))
+        print("requests_post_es got results of type(%s): (%s)" % (type(results).__name__, results))
     return results
 
-def urlopen_es(payload=None, pretty=PRETTY, text="fishing", verbose=0):
+def urllib_request_urlopen_es(payload=None, pretty=PRETTY, text="fishing", verbose=0):
     '''Send request to Elasticsearch using urlopen'''
     # esa_url = r"http://localhost:9200/_analyze?pretty=%s&analyzer=english&text=%s" % (pretty, text)
     esa_url = r"http://localhost:9200"
     res_byt = urllib.request.urlopen(esa_url).read()
     if verbose > 1:
-        print("urlopen_es got results of type(%s): (%s)" % (type(res_byt).__name__, res_byt))
+        print("urllib_request_urlopen_es got results of type(%s): (%s)" % (type(res_byt).__name__, res_byt))
     res_str = res_byt.decode('utf-8')
     res_dct = json.loads(res_str)
     if verbose > 0:
-        print("urlopen_es results after .decode('utf-8') and json.loads to dict:\n(%s)" % res_dct)
+        print("urllib_request_urlopen_es results after .decode('utf-8') and json.loads to dict:\n", res_dct)
     return res_dct
 
 
+def requests_get_es(payload=None, pretty=PRETTY, text="fishing", verbose=0):
+    '''post request to Elasticsearch'''
+    url = r"http://localhost:9200/_analyze?pretty=%s&analyzer=english&text=%s" % (pretty, text)
+    got = requests.get(url)
+    if verbose > 0:
+        print("requests_get_es got results of type(%s): (%s)" % (type(got).__name__, got))
+    return got.json()
 
-post_res = post_es()
-print("post_es results:")
+
+post_res = requests_post_es()
+print("requests_post_es results:")
 print(post_res)
 print()
-open_res = urlopen_es()
-print("urlopen_es results:")
+open_res = urllib_request_urlopen_es()
+print("urllib_request_urlopen_es results:")
 print(open_res)
+print()
+got_json = requests_get_es(verbose=1)
+print("requests_get_es got results with .json() -> dict:")
+print(got_json)
